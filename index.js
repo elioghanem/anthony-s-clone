e10371333e9ec3becd4997914287e9d448d44728
elioghanem <137090109+elioghanem@users.noreply.github.com>
2023-08-15 04:04:51 +0300

    fixing bugs

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-window.addEventListener("scroll", () => { //Adds event to run the following code when the page is scrolled
-    if (window.scrollY >= 100) { //Checks if the page is scrolled down 100px or more
-        document.querySelector(" .top__wrapper").classList.add(" .btn__show") //Adds the class btn__show when scrolled
-    } else { //If the page is not scrolled 100px down or more then run the following
-        document.querySelector(" .top__wrapper").classList.remove(" .btn__show") //Removes class btn__show if not scrolled
+// script.js
+const scrollButton = document.getElementById('top__wrapper');
+const specificHeight = 300; // Adjust this to your specific height in pixels
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= specificHeight) {
+        scrollButton.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none';
     }
+});
+
+scrollButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
