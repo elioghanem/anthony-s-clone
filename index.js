973fb56d576b2296c67a8c12e9b8cc152c663e0d
elioghanem <137090109+elioghanem@users.noreply.github.com>
2023-12-13 23:15:51 +0200

    adding a registration container when I  click on reg-btn in the login container and view password with a diff icon

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Add an event listener to the "HAVE AN ACCOUNT? LOGIN" element
+    let loginAccElement = document.querySelector(".login__acc");
+    loginAccElement.addEventListener("click", function() {
+        // Close the registration popup and open the login popup
+        closePopupReg();
+        openPopupLogin();
+    });
+});
+
 function openPopupLogin() {
     let popupLogin = document.getElementById("popupLogin");
     let overlay = document.getElementById("overlay");
@@ -12,4 +22,38 @@ function closePopupLogin() {
 
     popupLogin.classList.remove("open-popup__login");
     overlay.classList.remove("active");
+}
+
+function openPopupReg() {
+    closePopupLogin();
+    let popupReg = document.getElementById("popupReg");
+    let overlay = document.getElementById("overlay");
+
+    popupReg.classList.add("open-popup__reg");
+    overlay.classList.add("active");
+}
+
+function closePopupReg() {
+    let popupReg = document.getElementById("popupReg");
+    let overlay = document.getElementById("overlay");
+
+    popupReg.classList.remove("open-popup__reg");
+    overlay.classList.remove("active");
+}
+
+
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var eyeOpenIcon = document.getElementById("eye-open");
+    var eyeClosedIcon = document.getElementById("eye-closed");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeOpenIcon.style.display = "none";
+        eyeClosedIcon.style.display = "inline";
+    } else {
+        passwordInput.type = "password";
+        eyeOpenIcon.style.display = "inline";
+        eyeClosedIcon.style.display = "none";
+    }
 }
